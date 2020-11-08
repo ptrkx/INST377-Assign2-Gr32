674ecbe6ac0a98c705b1b5a29d0e8b1cf89cff01
ptrkx <70548323+ptrkx@users.noreply.github.com>
2020-11-08 09:15:35 -0500

    update script.js

--- design/script.js
+++ design/script.js
@@ -1,110 +1,40 @@
-function convertRestaurantsToCategories(restaurantList) {
-  // process your restaurants here!
-  const categoryArray = [];
-  const result = {};
-  for (let i = 0; i < restaurantList.length; i += 1) {
-    categoryArray.push(restaurantList[i].category);
+const pgData = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
 
-  }
-  for (let i = 0; i < categoryArray.length; i += 1) {
-    if (!result[categoryArray[i]]) {
-      result[categoryArray[i]] = 0;
+const cities = [];
+
+fetch(pgData)
+  .then(blob => blob.json())
+  .then(data => cities.push(...data))  /*... == spreading into the push!*/
+
+function findMatching(wordsToMatch, cities){
+  return cities.filter(place => {
+    const matches = new RegExp(wordsToMatch, 'gi'); 
+    /* g = global, i = insensitive */
+    if(wordsToMatch === '') {
+      return null;
     }
-    result[categoryArray[i]] += 1;
-  }
-  const reply = Object.keys(result).map((category) => ({
-    y: result[category],
-    label: category
-  }));
-  console.log('reply', reply);
-  return reply;
+    return place.city.match(matches) || place.state.match(matches) || place.name.match(matches);
+  });
 }
 
-function makeYourOptionsObject(datapointsFromRestaurantsList) {
-  // set your chart configuration here!
-  console.log("makeYourOptionsObject");
-  CanvasJS.addColorSet('customColorSet1', [
-    // add an array of colors here https://canvasjs.com/docs/charts/chart-options/colorset/
-    "#2F4F4F",
-    "#008080",
-    "#2E8B57"
-  ]);
-
-  return {
-    animationEnabled: true,
-    colorSet: 'customColorSet1',
-    title: {
-      text: 'Places To Eat Out In Future'
-    },
-    axisX: {
-      interval: 1,
-      labelFontSize: 12
-    },
-    axisY2: {
-      interlacedColor: 'rgba(1,77,101,.2)',
-      gridColor: 'rgba(1,77,101,.1)',
-      title: 'Restaurants by Category',
-      labelFontSize: 12,
-      scaleBreaks: {customBreaks: [
-        {
-          type: "zigzag",
-          startValue: 40,
-          endValue: 50,
-          color: "red"
-        },
-        {
-          type: "zigzag",
-          startValue: 85,
-          endValue: 100,
-          color: "red"
-        },
-        {
-          type: "zigzag",
-          startValue: 140,
-          endValue: 175,
-          color: "red"
-        }
-
-      ]} // Add your scale breaks here https://canvasjs.com/docs/charts/chart-options/axisy/scale-breaks/custom-breaks/
-    },
-    data: [{
-      type: 'bar',
-      name: 'restaurants',
-      axisYType: 'secondary',
-      dataPoints: datapointsFromRestaurantsList
-    }]
-  };
+function displayMatches() {
+  const matchingArray = findMatching(this.value, cities);
+  const html = matchingArray.map(place => {
+    return `
+      <li>
+        <span class = "name">${place.name}</span>
+        <span class = "location">${place.city}, ${place.state}, ${place.zipcode}</span>
+        <span class = "category">${place.category}</span>
+      </li>
+    `;
+  }).join('');
+  suggestions.innerHTML = html;
 }
 
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
 
-function runThisWithResultsFromServer(jsonFromServer) {
-  console.log('jsonFromServer', jsonFromServer);
-  sessionStorage.setItem('restaurantList', JSON.stringify(jsonFromServer)); // don't mess with this, we need it to provide unit testing support
-  // Process your restaurants list
-  // Make a configuration object for your chart
-  // Instantiate your chart
-  const reorganizedData = convertRestaurantsToCategories(jsonFromServer);
-  const options = makeYourOptionsObject(reorganizedData);
-  
-  const chart = new CanvasJS.Chart('chartContainer', options);
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
 
-  chart.render();
-}
 
-// Leave lines 52-67 alone; do your work in the functions above
-document.body.addEventListener('submit', async (e) => {
-  e.preventDefault(); // this stops whatever the browser wanted to do itself.
-  const form = $(e.target).serializeArray();
-  fetch('/api', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(form)
-  })
-    .then((fromServer) => fromServer.json())
-    .then((jsonFromServer) => runThisWithResultsFromServer(jsonFromServer))
-    .catch((err) => {
-      console.log(err);
-    });
-});
